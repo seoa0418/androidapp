672d2467199c14d299551c86fee8f4a8899cd7e0
seoaj <seoajjang06@naver.com>
2025-10-29 19:19:55 +0900

    new

--- mobileapp-main/w04/w042/app/src/main/java/com/example/w04/MainActivity.kt
+++ mobileapp-main/w04/w042/app/src/main/java/com/example/w04/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.w04
 
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -16,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         val nameText: TextView = findViewById(R.id.nameText)
         val introText: TextView = findViewById(R.id.introText)
 
-        // 이미지, 이름, 자기소개 변경 가능
+// 이미지, 이름, 자기소개 변경 가능
         profileImage.setImageResource(R.drawable.ic_launcher_foreground)
         nameText.text = "김서아"
-        introText.text = "앱 개발을 배우고있는 김서아 입니다."
+        introText.text = "앱 개발을 배우고있는 김서아 입니다. 앞으로 배울 내용이 기대됩니다!"
     }
 }
